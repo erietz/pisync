0950574e021a03b44f71bacdb14ceb1ee6b9b5d8
Ethan Rietz <ewrietz@gmail.com>
2022-05-24 21:56:31 +0000

    actually use my script

--- run_backups.py
+++ run_backups.py
@@ -1,13 +1,22 @@
 from config import Config
 from backup import backup
 
-config = Config(
-    source_dir="/home/ethan/Documents",
-    destination_dir="/tmp/backup_test",
+# will need to be root to run rsync for a different user
+home_directories = Config(
+    source_dir="/home/",
+    destination_dir="/media/backup_drive_linux/home_directory_backups/",
     exclude_file_patterns=[
-        "from-usb-sticks",
-        "discord",
+        "/home/*/.cache/",          # no need for cached files
+        "/home/*/.local/",          # nvim plugins and python packages are huge
+        "/home/*/.npm/",            # what is the crap in here?
+        "**/node_modules/",         # yikes
     ]
 )
 
-backup(config)
+large_harddrive = Config(
+    source_dir="/mnt/hd2",
+    destination_dir="/media/backup_drive_linux/hd2_backups/"
+)
+
+backup(home_directories)
+backup(large_harddrive)
