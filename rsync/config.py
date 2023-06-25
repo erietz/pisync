5570f04d26ec731a261cf4afda370a9ff45d5d5c
erietz <ewrietz@gmail.com>
2023-06-25 17:34:23 -0500

    add docstrings to base class

--- rsync/config.py
+++ rsync/config.py
@@ -23,24 +23,41 @@ class _BaseConfig(ABC):
         exclude_file_patterns: List[str] = None,
         log_file: str = Path.home() / ".local/share/backup/rsync-backups.log"
     ):
+        """
+        :raises:
+            Exception: If the configuration is invalid.
+        """
         pass
 
     @staticmethod
     @abstractmethod
     def ensure_dir_exists(path: str) -> Path:
+        """
+        :returns: The Path object from the input path str
+        :raises:
+            InvalidPath: If path does not exist or is not a directory
+        """
         pass
 
     @abstractmethod
     def generate_new_backup_dir_path(self) -> Path:
+        """
+        :returns: The Path object of the directory where the new backup will be
+        written.
+        :raises:
+            InvalidPath: If the destination directory already exists
+        """
         pass
 
     @abstractmethod
     def get_rsync_command(self) -> List[str]:
+        """
+        :returns: The formatted rsync command line arguments from the Config.
+        """
         pass
 
 
 class LocalConfig(_BaseConfig):
-    """Configuration for rsync backups"""
     def __init__(
         self,
         source_dir: str,
